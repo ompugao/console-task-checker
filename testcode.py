2485bffc3bb082ec511e43ccb78af7808edd174b
alice <genocidedragon@gmail.com>
2012-03-18 20:43:21 +0900

    changed file setting

--- testcode.py
+++ testcode.py
@@ -4,6 +4,7 @@
 import unittest
 import cPickle as pickle
 import os
+import commands
 import json
 
 class Test_TaskChecker(unittest.TestCase):
@@ -11,30 +12,25 @@ class Test_TaskChecker(unittest.TestCase):
 		pass
 
 	def test_add_function(self):
-		os.system("python taskcheck.py --add 2012/03/20 確定申告")
-		os.system("python taskcheck.py --add 2012/03/30 テストテスト")
-		os.system("python taskcheck.py --add 2013/01/01 元旦")
-		os.system("python taskcheck.py --add 2012/04/15 テスト")
+		os.system("python task.py add 2012/03/20 確定申告")
+		os.system("python task.py add 2012/03/30 テストテスト")
+		os.system("python task.py add 2013/01/01 元旦")
+		os.system("python task.py add 2012/04/15 テスト")
 
-	def test_show_tasklist(self):
-		print "="*50
-		os.system("python taskcheck.py --show")
-
-	def test_show_tasklist_on_date(self):
-		print "="*50
-		os.system("python taskcheck.py --show 2012/04")
-		print "="*50
-		os.system("python taskcheck.py --show 2012/03/20")
-		print "="*50
+#	def test_show_tasklist(self):
+#		print "="*50
+#		os.system("python task.py --show")
+#
+#	def test_show_tasklist_on_date(self):
+#		print "="*50
+#		os.system("python task.py --show 2012/04")
+#		print "="*50
+#		os.system("python task.py --show 2012/03/20")
+#		print "="*50
 		
-	def test_show_list_length(self):
-		container = pickle.load(open(os.path.expanduser("~/.task_container")))
-		print container.length
-
-
-
-
-
+	def test_delete_task(self):
+		print "-"*100
+		os.system("python task.py delete 1")
 
 if __name__ == "__main__":
 	suite = unittest.TestLoader().loadTestsFromTestCase(Test_TaskChecker)
